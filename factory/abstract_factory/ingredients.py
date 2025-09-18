dfce993cc6703af9887472b44b0ef16ef22ad29f
Mauricio <omkreszczuk@estudiantes.unsam.edu.ar>
2025-09-18 13:08:15 -0300

    Paso 2

--- factory/abstract_factory/ingredients.py
+++ factory/abstract_factory/ingredients.py
@@ -17,6 +17,42 @@ class Clams:
     def __init__(self, name): self.name=name;  
     def __str__(self): return self.name
 
+# #########################
+# VEGETALES
+class Veggie(ABC):
+    @abstractmethod
+    def __str__(self):
+        ...
+#concretas:
+class Garlic(Veggie):
+    def __str__(self): return "Garlic"
+
+class Onion(Veggie):
+    def __str__(self): return "Onion"
+
+class Mushroom(Veggie):
+    def __str__(self): return "Mushroom"
+
+class RedPepper(Veggie):
+    def __str__(self): return "Red Pepper"
+
+class BlackOlives(Veggie):
+    def __str__(self): return "Black Olives"
+
+class Spinach(Veggie):
+    def __str__(self): return "Spinach"
+
+class Eggplant(Veggie):
+    def __str__(self): return "Eggplant"
+
+# Pepperoni
+class Pepperoni(ABC):
+    @abstractmethod
+    def __str__(self):
+        ...
+class SlicedPepperoni(Pepperoni):
+    def __str__(self): return "Sliced Pepperoni"
+
 # Abstract Factory
 class PizzaIngredientFactory(ABC):
     @abstractmethod
@@ -27,16 +63,42 @@ class PizzaIngredientFactory(ABC):
     def create_cheese(self) -> Cheese: ...
     @abstractmethod
     def create_clam(self) -> Clams: ...
+    
+    #2
+    @abstractmethod
+    def create_veggies(self) -> list[Veggie]: ...
+    @abstractmethod
+    def create_pepperoni(self) -> Pepperoni: ...
 
 # Concrete factories
 class NYPizzaIngredientFactory(PizzaIngredientFactory):
-    def create_dough(self) -> Dough:  return Dough("Thin Crust Dough")
-    def create_sauce(self) -> Sauce:  return Sauce("Marinara Sauce")
-    def create_cheese(self) -> Cheese:return Cheese("Reggiano Cheese")
-    def create_clam(self) -> Clams:   return Clams("Fresh Clams")
+    def create_dough(self) -> Dough:
+        return Dough("Thin Crust Dough")
+    def create_sauce(self) -> Sauce:
+        return Sauce("Marinara Sauce")
+    def create_cheese(self) -> Cheese:
+        return Cheese("Reggiano Cheese")
+    def create_clam(self) -> Clams:
+        return Clams("Fresh Clams")
+    
+    #3
+    def create_veggies(self) -> list[Veggie]:
+        return [Garlic(), Onion(), Mushroom(), RedPepper()]
+    def create_pepperoni(self) -> Pepperoni:
+        return SlicedPepperoni()
 
 class ChicagoPizzaIngredientFactory(PizzaIngredientFactory):
-    def create_dough(self) -> Dough:  return Dough("Thick Crust Dough")
-    def create_sauce(self) -> Sauce:  return Sauce("Plum Tomato Sauce")
-    def create_cheese(self) -> Cheese:return Cheese("Mozzarella Cheese")
-    def create_clam(self) -> Clams:   return Clams("Frozen Clams")
+    def create_dough(self) -> Dough:
+        return Dough("Thick Crust Dough")
+    def create_sauce(self) -> Sauce:
+        return Sauce("Plum Tomato Sauce")
+    def create_cheese(self) -> Cheese:
+        return Cheese("Mozzarella Cheese")
+    def create_clam(self) -> Clams:
+        return Clams("Frozen Clams")
+    
+    #3
+    def create_veggies(self) -> list[Veggie]:
+        return [BlackOlives(), Spinach(), Eggplant()]
+    def create_pepperoni(self) -> Pepperoni:
+        return SlicedPepperoni()
--- factory/abstract_factory/main.py
+++ factory/abstract_factory/main.py
@@ -1,11 +1,23 @@
 from .store import NYPizzaStore, ChicagoPizzaStore
 
 def main():
-    ny = NYPizzaStore(); chi = ChicagoPizzaStore()
-    print("*"*25, "ABSTRACT", "*"*25)
-    ny.order_pizza("cheese")
-    print("*"*50)
-    chi.order_pizza("clam")
+    print("*"*25, "ABSTRACT FACTORY", "*"*25)
+    ny = NYPizzaStore()
+    chi = ChicagoPizzaStore()
+
+# NY
+    print("\n--- New York Orders ---")
+    for kind in ["cheese", "clam", "veggie", "pepperoni"]:
+        pizza = ny.order_pizza(kind)
+        print("Ethan ordered:", pizza)
+        print("-"*40)
+
+# Chicago
+    print("\n--- Chicago Orders ---")
+    for kind in ["cheese", "clam", "veggie", "pepperoni"]:
+        pizza = chi.order_pizza(kind)
+        print("Joel ordered:", pizza)
+        print("-"*40)
 
 if __name__ == "__main__":
     main()
