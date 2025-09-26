04f9862186d168a9dd6c38eaee38676754f033cd
Mauricio <omkreszczuk@estudiantes.unsam.edu.ar>
2025-09-26 10:07:58 -0300

    factory

---
+++ factory/factory_method/main.py
@@ -0,0 +1,33 @@
+from .store import NYPizzaStore, ChicagoPizzaStore
+
+def main():
+    print("*" * 20, "FACTORY_METHOD", "*" * 25)
+
+    ny = NYPizzaStore()
+    chi = ChicagoPizzaStore()
+
+    # NY pizzas
+    print("\n--- New York Orders ---")
+    pedidos =["cheese", "veggie", "pepperoni"]
+    for orden in pedidos:
+        pizza = ny.order_pizza(orden)
+        print("Ethan ordered:", pizza)
+        print("-" * 40)
+
+    # Chicago pizzas
+    print("\n--- Chicago Orders ---")
+    pedidos = ["cheese", "veggie", "pepperoni"]
+    for orden in pedidos :
+        pizza = chi.order_pizza(orden)
+        print("Joel ordered:", pizza)
+        print("-" * 40)
+
+
+if __name__ == "__main__":
+    main()
+
+'''
+
+python -m factory.factory_method.main
+
+'''
---
+++ factory/factory_method/pizza.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+class Pizza(ABC):
+    name: str = "Generic Pizza"
+    toppings: list[str] = []
+
+    def prepare(self):
+        print(f"Preparing {self.name}")
+        print("Adding toppings:", ", ".join(self.toppings))
+
+    def bake(self): print("Bake 25 min at 350")
+    def cut(self):  print("Cutting pizza into diagonal slices")
+    def box(self):  print("Place pizza in official box")
+    def __str__(self): return self.name
+
+class NYStyleCheesePizza(Pizza):
+    def __init__(self):
+        self.name="NY Style Sauce & Cheese"; self.toppings=["Reggiano cheese"]
+
+class ChicagoStyleCheesePizza(Pizza):
+    def __init__(self):
+        self.name="Chicago Style Deep Dish Cheese"; self.toppings=["Shredded Mozzarella"]
+    def cut(self): print("Cutting the pizza into square slices")
+#las de chicago se cortan en cuadrados
+
+######### PASO 1 ############
+
+class NYStyleVeggiePizza(Pizza):
+    def __init__(self):
+        self.name = "NY Style Veggie Pizza"
+        self.toppings = ["Garlic", "Onion", "Mushroom", "Red Pepper"]
+
+class NYStylePepperoniPizza(Pizza):
+    def __init__(self):
+        self.name = "NY Style Pepperoni Pizza"
+        self.toppings = ["Pepperoni", "Reggiano Cheese"]
+
+class ChicagoStyleVeggiePizza(Pizza):
+    def __init__(self):
+        self.name = "Chicago Style Veggie Pizza"
+        self.toppings = ["Black Olives", "Spinach", "Eggplant"]
+
+    def cut(self):
+        print("Cutting the pizza into square slices")
+
+class ChicagoStylePepperoniPizza(Pizza):
+    def __init__(self):
+        self.name = "Chicago Style Pepperoni Pizza"
+        self.toppings = ["Sliced Pepperoni", "Mozzarella Cheese"]
+
+    def cut(self):
+        print("Cutting the pizza into square slices")
